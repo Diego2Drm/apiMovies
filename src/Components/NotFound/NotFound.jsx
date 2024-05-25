3e079011655db9b217a0466737bdfe17d643f786
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-05-24 19:26:30 -0600

    NotFound Responsive

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -6,11 +6,11 @@ function NotFound() {
   return (
     <>
       <HeaderPage />
-      <section className="w-full h-screen flex items-center justify-center text-[5rem] text-DarkBlue font-semibold">
+      <section className="w-full h-screen flex flex-col items-center pt-32 md:justify-center md:pt-0 md:flex-row text-DarkBlue font-semibold px-32">
         <div className="text-[10rem]">
           <ion-icon name="alert-circle-sharp"></ion-icon>
         </div>
-        <p>
+        <p className="text-center text-3xl md:text-[4rem] md:text-start md:leading-[6.5rem]">
           Lo Sentimos No Encontramos Información Sobre Esta Película... <br />
           Intenta Con Otra.
         </p>
